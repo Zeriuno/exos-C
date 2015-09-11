2f4701bfd50af064b55f0b7465f74fd9be016db5
Daniele Pitrolo <d.pitrolo@gmx.com>
2015-09-11 09:15:34 +0200

    Renommage

---
+++ 9-08-remplacement-scanf-un-seul.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+main()
+{
+  int a,b,somme;
+
+  printf("Entrez deux valeurs (ex. 12 24) : ");
+  scanf("%d %d",&a, &b);
+  somme = a+b;
+  printf("somme de %d + %d = %d\n",a,b,somme);
+}
