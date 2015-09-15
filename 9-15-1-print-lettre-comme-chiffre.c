6638da57f4643f80ccb0e5947a5df06fd5879572
Daniele Pitrolo <d.pitrolo@gmx.com>
2015-09-15 18:55:15 +0200

    Sans scanf, affectation statique

--- 9-15-1-print-lettre-comme-chiffre.c
+++ 9-15-1-print-lettre-comme-chiffre.c
@@ -4,7 +4,8 @@
 main()
 {
   char lettre            ;
-  scanf("%c", &lettre)   ;
+  lettre = 'A'           ;
+  /*scanf("%c", &lettre)   ;*/
   printf("%c\n", lettre) ;
   printf("%d\n", lettre) ;
 }
