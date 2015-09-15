476c618de86bc3bd274226c47aa9fa87f05a0aa2
Daniele Pitrolo <d.pitrolo@gmx.com>
2015-09-15 18:43:05 +0200

    conversion d'une valeur entre différents systèmes

---
+++ 9-15-3-conversion-valeur.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+main()
+{
+  int i                            ;
+
+  printf("Saisissez un nombre : ") ;
+  scanf("%d", &i)                  ;
+  printf("%d\n", i)                ;
+  printf("%o\n", i)                ;
+  printf("%x\n", i)                ;
+  printf("%X\n", i)                ;
+}
