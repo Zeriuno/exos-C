8c3667ffc3169445583652b381e87ff9bf9b089a
Daniele Pitrolo <d.pitrolo@gmx.com>
2015-09-15 17:42:15 +0200

    test sur l'encodage de l'information de la variable char

---
+++ 9-15-2-char-avec-sans-signe.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+main()
+{
+  unsigned char c1 ; /*code de M. Léry*/
+  signed char c2 ; /*code de M. Léry*/
+  char c3 ;
+
+  c1 = -1 ; /*code de M. Léry*/
+  c2 = -1 ; /*code de M. Léry*/
+  c3 = -1 ;
+
+  printf("%d\n", c1) ; /*code de M. Léry*/
+  printf("%d\n", c2) ; /*code de M. Léry*/
+  printf("%d\n", c3) ;
+}
