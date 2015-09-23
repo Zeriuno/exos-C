e7a5036b992dd04df8603be658dead8af81130ab
Daniele Pitrolo <d.pitrolo@gmx.com>
2015-09-23 19:01:09 +0200

    Logiciel d'accès à un fichier liste.txt et stockage en variables de son contenu

--- 9-23-1-accès-fichier.c
+++ 9-23-1-accès-fichier.c
@@ -2,14 +2,14 @@
 #include <stdio.h>
 main()
 {
-  char nom[100], prenom[200]   ;
-  int age                      ;
-  FILE *f1                     ;
+  char nom[100], prenom[200]             ;
+  int age                                ;
+  FILE *f1                               ;
 
-  f1 = fopen("liste.txt", "r") ;
-  fscanf(f1, "%s", nom)        ;
-  fscanf(f1, "%s", prenom)     ;
-  fscanf(f1, "%d",&age)        ;
-  fclose(f1)                   ;
+  f1 = fopen("liste.txt", "r")           ;
+  fscanf(f1, "%s", nom)                  ;
+  fscanf(f1, "%s", prenom)               ;
+  fscanf(f1, "%d",&age)                  ;
+  fclose(f1)                             ;
   printf("%s %s %d\n", nom, prenom, age) ;
 }
