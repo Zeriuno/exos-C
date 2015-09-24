ca4772f95a01e05b5a5d1ed8154abc2f76677294
Daniele Pitrolo <d.pitrolo@gmx.com>
2015-09-24 17:26:17 +0200

    Exercices écriture de fichier à partir de la saisie

---
+++ 9-24-1-ecrire-fichier.c
@@ -0,0 +1,20 @@
+/* exercice de cours fait par M. Léry */
+
+#include <stdio.h>
+main()
+{
+  string nom[100], prenom[100]     ;
+  int age                          ;
+  printf("Écrire le nom : ")       ;
+  scanf("%s", nom)                 ;
+  printf("Écrire le prénom : ")    ;
+  scanf("%s", prenom)              ;
+  printf("Écrire l'âge : ")        ;
+  scanf("%d", age)                 ;
+  f1=fopen("listesaisie.txt", "w") ;
+  /*un fichier qui est ouvert est écrasé, si il existé avant, ou crée, s'il n'existait pas.
+  Si un fichier existait, avait des données et est ouvert et fermé, il ne contiendra plus rien*/
+  fprintf(f1, "%s %s %d\n", nom, prenom, age)
+  fclose(f1)                       ;
+
+}
---
+++ 9-24-2-ecrire-nommer-fichier.c
@@ -0,0 +1,24 @@
+/* exercice de cours fait par M. Léry */
+
+#include <stdio.h>
+main()
+{
+  char nom[100], prenom[100], nom_fichier[200]          ;
+  int age                                               ;
+  FILE *f1                                              ;
+  printf("Écrire le nom : ")                            ;
+  scanf("%s", nom)                                      ;
+  printf("Écrire le prénom : ")                         ;
+  scanf("%s", prenom)                                   ;
+  printf("Écrire l'âge : ")                             ;
+  scanf("%d",&age)                                      ;
+  printf("Écrire le nom du fichier (avec extension): ") ;
+  scanf("%s", nom_fichier)                              ;
+  f1=fopen(nom_fichier, "w") ;
+
+  /*un fichier qui est ouvert est écrasé, si il existé avant, ou crée, s'il n'existait pas.
+  Si un fichier existait, avait des données et est ouvert et fermé, il ne contiendra plus rien*/
+
+  fprintf(f1, "%s %s %d\n", nom, prenom, age)           ;
+  fclose(f1)                                            ;
+}
