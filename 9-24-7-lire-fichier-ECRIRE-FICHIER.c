034785176f84275ad2567bb4f637d22d5e8ca0c3
Daniele Pitrolo <d.pitrolo@gmx.com>
2015-09-24 18:48:06 +0200

    Traitement toupper fait sur le contenu du fichier et sur le nom du fichier

---
+++ 9-24-7-lire-fichier-ECRIRE-FICHIER.c
@@ -0,0 +1,36 @@
+/*exercice de M.Léry*/
+
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+main()
+{
+  char nom[100], prenom[100], fichier_entree[100], fichier_sortie[100] ;
+  int age, taille, i ;
+  FILE *f1; /*réduction à une seule variable fichier*/
+
+  printf("Nom du fichier à lire : ")        ;
+  scanf("%s", fichier_entree)               ;
+  f1=fopen(fichier_entree, "r")             ;
+  fscanf(f1, "%s %s %d", nom, prenom, &age) ;
+  fclose(f1)                                ;
+  taille = strlen(nom)                      ; /*code de M.Léry d'ailleurs, mis ici par moi*/
+  for (i=0 ; i < taille ; i ++)
+  {
+     nom[i] = toupper(nom[i])               ;
+   } /*code de M.Léry d'ailleurs, mis ici par moi*/
+  taille = strlen(prenom)                   ; /*code de M.Léry d'ailleurs, mis ici par moi*/
+  for (i=0 ; i < taille ; i ++)
+  {
+    prenom[i] = toupper(prenom[i])          ;
+  } /*code de M.Léry d'ailleurs, mis ici par moi*/
+  taille = strlen(fichier_entree) ;
+  for (i=0 ; i < taille ; i ++)
+  {
+    fichier_entree[i] = toupper(fichier_entree[i]) ;
+  }/*code de M.Léry d'ailleurs, mis ici par moi*/
+  printf("Le nom du fichier sauvegardé est %s\n", fichier_entree) ; /*code de moi*/
+  f1=fopen(fichier_entree, "w")             ;
+  fprintf(f1, "%s %s %d\n", nom, prenom, age) ;
+  fclose(f1)                                ;
+}
