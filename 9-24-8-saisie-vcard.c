bcd1e59ecefd378031477ca65829f5ad7e143e40
Daniele Pitrolo <d.pitrolo@gmx.com>
2015-09-25 17:35:50 +0200

    Génie logiciel

--- 9-24-8-saisie-vcard.c
+++ 9-24-8-saisie-vcard.c
@@ -3,8 +3,8 @@
 main()
 {
   char prenom[100], nom[100], mel[100], fichier[204] ;
-  int tel ;
-  FILE *vcard ;
+  int tel                                            ;
+  FILE *vcard                                        ;
 
   printf("              ****************\n") ;
   printf("              ** ATTENTION! **\n") ;
@@ -20,19 +20,19 @@ main()
   printf("Tu vas écrire les informations\n")     ;
   printf("Je vais les réunir dans un fichier\n") ;
 
-  printf("Écris le prénom de la personne : ") ;
-  scanf("%s", prenom) ;
-  printf("Écris son nom : ") ;
-  scanf("%s", nom) ;
-  printf("Écris son adresse mél : ") ;
-  scanf("%s", mel) ;
+  printf("Écris le prénom de la personne : ")    ;
+  scanf("%s", prenom)                            ;
+  printf("Écris son nom : ")                     ;
+  scanf("%s", nom)                               ;
+  printf("Écris son adresse mél : ")             ;
+  scanf("%s", mel)                               ;
   printf("Écris son numéro de téléphone (je mets le +, tu mets le code pays, et pas d'espaces) : ") ;
-  scanf("%d", &tel) ;
-  strcat(fichier,nom);
-  strcat(fichier,prenom)     ;
-  strcat(fichier,".vcf") ;
-  vcard = fopen(fichier, "w")    ;
+  scanf("%d", &tel)                              ;
+  strcat(fichier,nom)                            ;
+  strcat(fichier,prenom)                         ;
+  strcat(fichier,".vcf")                         ;
+  vcard = fopen(fichier, "w")                    ;
   fprintf(vcard, "BEGIN:VCARD\nVERSION:3.0\nN:%s;%s;;;\nFN:%s %s\nORG:\nROLE:\nEMAIL;type=INTERNET:%s\nTEL;type=VOICE:+%d\nNOTE:\nEND:VCARD", nom, prenom, prenom, nom, mel, tel) ;
-  fclose(vcard) ;
+  fclose(vcard)  ;
   printf("Le fichier %s a bien été créé\n", fichier) ;
 }
