d78b498f952213e60ba26bcd387b6d561be69e79
Daniele Pitrolo <d.pitrolo@gmx.com>
2015-09-28 22:00:35 +0200

    Exercices de Vendredi 25 Septembre

---
+++ 9-25-3-entier-vs-reel.c
@@ -0,0 +1,24 @@
+/*exercice fait par M. Léry*/
+
+#include <stdio.h>
+main()
+{
+  float nombre, partie_decimale ;
+  int partie_entiere            ;
+  printf("Saisir un nombre : ") ;
+  scanf("%f",&nombre)           ;
+
+  partie_entiere = nombre       ;
+
+  if ((nombre - partie_entiere) == 0)
+  {
+    printf("Le nombre saisi est un entier\n") ;
+  }
+  else
+  {
+    partie_decimale = nombre - partie_entiere ;
+    printf("C'est un réel.\n") ;
+    printf("Sa partie entière est  :%d\n", partie_entiere) ;
+    printf("Sa partie décimale est :%4.2f\n", partie_decimale) ;
+  }
+}
---
+++ 9-25-6-lettre-ascii.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+main()
+{
+  char lettre, poubelle ;
+  int  ascii            ;
+
+printf("0 si tu veux le code d'une lettre\n1 si tu veux la lettre d'un code : ") ;
+scanf("%d", &ascii) ;
+if(!ascii)
+  {
+  scanf("%c", &poubelle) ;
+  while(poubelle(getchar()) != '\n') ;
+  scanf("%c", &lettre)   ;
+  printf("%d\n", lettre) ;
+  }
+else
+  {
+  scanf("%d",&ascii);
+  lettre = ascii ;
+  printf("%c\n", lettre) ;
+  }
+}
