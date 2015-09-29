ac544d898d666949e1464699a9144b5e9de3c0b7
Daniele Pitrolo <d.pitrolo@gmx.com>
2015-09-29 18:42:33 +0200

    scanf auquel est affectée une valeur booléenne pour vérifier la saisie - exométro

---
+++ 9-29-3-controle-scanf.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+main()
+{
+  int entier, verif                     ;
+  printf("Saisir un chiffre entier : ") ;
+  verif=scanf("%d", &entier)            ;
+  printf("Le chiffre est %d, la vérification est %d\n", entier, verif) ;  
+}
