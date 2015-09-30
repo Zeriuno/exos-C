36675f0aa4588f7ac7d5501044c20cbc5886c969
Daniele Pitrolo <d.pitrolo@gmx.com>
2015-09-30 22:01:04 +0200

    Des exercices restés ignorés

--- 9-24-7-lire-fichier-ECRIRE-FICHIER.c
+++ 9-24-7-lire-fichier-ECRIRE-FICHIER.c
@@ -9,28 +9,28 @@ main()
   int age, taille, i ;
   FILE *f1; /*réduction à une seule variable fichier*/
 
-  printf("Nom du fichier à lire : ")        ;
-  scanf("%s", fichier_entree)               ;
-  f1=fopen(fichier_entree, "r")             ;
-  fscanf(f1, "%s %s %d", nom, prenom, &age) ;
-  fclose(f1)                                ;
-  taille = strlen(nom)                      ; /*code de M.Léry d'ailleurs, mis ici par moi*/
+  printf("Nom du fichier à lire : ")                              ;
+  scanf("%s", fichier_entree)                                     ;
+  f1=fopen(fichier_entree, "r")                                   ;
+  fscanf(f1, "%s %s %d", nom, prenom, &age)                       ;
+  fclose(f1)                                                      ;
+  taille = strlen(nom)                                            ; /*code de M.Léry d'ailleurs, mis ici par moi*/
   for (i=0 ; i < taille ; i ++)
   {
-     nom[i] = toupper(nom[i])               ;
+     nom[i] = toupper(nom[i])                                     ;
    } /*code de M.Léry d'ailleurs, mis ici par moi*/
-  taille = strlen(prenom)                   ; /*code de M.Léry d'ailleurs, mis ici par moi*/
+  taille = strlen(prenom)                                         ; /*code de M.Léry d'ailleurs, mis ici par moi*/
   for (i=0 ; i < taille ; i ++)
   {
-    prenom[i] = toupper(prenom[i])          ;
+    prenom[i] = toupper(prenom[i])                                ;
   } /*code de M.Léry d'ailleurs, mis ici par moi*/
-  taille = strlen(fichier_entree) ;
+  taille = strlen(fichier_entree)                                 ;
   for (i=0 ; i < taille ; i ++)
   {
-    fichier_entree[i] = toupper(fichier_entree[i]) ;
+    fichier_entree[i] = toupper(fichier_entree[i])                ;
   }/*code de M.Léry d'ailleurs, mis ici par moi*/
   printf("Le nom du fichier sauvegardé est %s\n", fichier_entree) ; /*code de moi*/
-  f1=fopen(fichier_entree, "w")             ;
-  fprintf(f1, "%s %s %d\n", nom, prenom, age) ;
-  fclose(f1)                                ;
+  f1=fopen(fichier_entree, "w")                                   ;
+  fprintf(f1, "%s %s %d\n", nom, prenom, age)                     ;
+  fclose(f1)                                                      ;
 }
