f1b4654297c2337c568c3ac33b6f8f5d6a2c3b81
Daniele Pitrolo <d.pitrolo@gmx.com>
2015-10-09 18:34:03 +0200

    Alignement des point-virgules

--- 10-9-1-majuscule-minuscule.c
+++ 10-9-1-majuscule-minuscule.c
@@ -8,10 +8,10 @@
 #include <stdio.h>
 main()
 {
-  char majuscule, minuscule ;
-  printf("Saisir une lettre majuscule : ") ;
-  scanf("%c", &majuscule) ;
+  char majuscule, minuscule                                                    ;
+  printf("Saisir une lettre majuscule : ")                                     ;
+  scanf("%c", &majuscule)                                                      ;
   printf("Merci!\nJe vais maintenant afficher la même lettre en minuscule.\n") ;
-  minuscule = majuscule + 32 ;
-  printf("La majuscule correspondante est %c\n", minuscule) ;
+  minuscule = majuscule + 32                                                   ;
+  printf("La majuscule correspondante est %c\n", minuscule)                    ;
 }
