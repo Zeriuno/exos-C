b9ab108242eff71ca9a05ad44c12144ce467bfa7
Daniele Pitrolo <d.pitrolo@gmx.com>
2015-11-02 20:07:34 +0100

    Convertir un chiffre donné en base décimale dans une base au choix - exocours

---
+++ 11-2-5-transcodage-de-bases.c
@@ -0,0 +1,36 @@
+/* Exercice fait par M. Léry*/
+#include <stdio.h>
+#include <string.h>
+main()
+{
+  int chiffre, base, quotient, reste         ;
+  char lettre, chlettre[20], affichage[20]   ;
+
+  printf("Saisir le chiffre à convertir : ") ;
+  scanf("%d", &chiffre)                      ;
+  printf("Saisir la base : ")                ;
+  scanf("%d", &base)                         ;
+  quotient = -1                 ;
+  affichage[0] = '\0'           ;
+  while (quotient != 0)
+  {
+    reste =  chiffre % base     ;
+    quotient = chiffre / base   ;
+    chiffre = quotient          ;
+    if ((reste >= 0) && (reste <= 9))
+    {
+      printf("%d\n", reste)     ;
+      lettre = reste + 48       ;
+    }
+    else
+    {
+      lettre = reste + 55       ;
+      printf("%c\n", lettre)    ;
+    }
+    chlettre[0] = lettre        ;
+    chlettre[1] = '\0'          ;
+    strcat(chlettre, affichage) ;
+    strcpy(affichage, chlettre) ;
+  }
+  printf("%s\n", affichage)     ;
+}
