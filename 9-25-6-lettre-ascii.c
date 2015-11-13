6271f4c14a2b9e06a38460722390256e647e8865
Daniele Pitrolo <d.pitrolo@gmx.com>
2015-11-13 09:31:07 +0100

    Réparation du switch, reste à faire marcher la poubelle

--- 9-25-6-lettre-ascii.c
+++ 9-25-6-lettre-ascii.c
@@ -1,25 +1,32 @@
-/*-------------------------------------------------------------
--  le switch prédisposé ne fonctionne pas du tout: à réparer --
+/*--------------------------------------------------------------
+--  le switch prédisposé ne fonctionne pas du tout: à réparer --
+--  2015-11-13 réparé, reste à faire marcher la poubelle      --
 -------------------------------------------------------------*/
 #include <stdio.h>
 main()
 {
   char lettre, poubelle ;
-  int  ascii            ;
+  int  ascii, choix     ;
 
-printf("0 si tu veux le code d'une lettre\n1 si tu veux la lettre d'un code : ") ;
-scanf("%d", &ascii) ;
-if(!ascii)
+printf("1 si tu veux le code d'une lettre\n2 si tu veux la lettre d'un code : ") ;
+scanf("%d", &choix) ;
+switch (choix)
   {
+  case 1 :
+  scanf("%d", &ascii) ;
+
   scanf("%c", &poubelle) ;
   while((poubelle=getchar()) != '\n') ;
   scanf("%c", &lettre)   ;
   printf("%d\n", lettre) ;
-  }
-else
-  {
-  scanf("%d",&ascii);
-  lettre = ascii ;
-  printf("%c\n", lettre) ;
+   break ;
+  case 2 :
+    scanf("%d",&ascii);
+    lettre = ascii ;
+    printf("%c\n", lettre) ;
+    break ;
+  default:
+    printf("Merci, au revoir!");
+    break;
   }
 }
