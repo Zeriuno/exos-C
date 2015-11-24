4a1ce3c6930ff241efaa2401b6934362e7d9bf4f
Daniele Pitrolo <d.pitrolo@gmx.com>
2015-11-24 09:09:05 +0100

    Commentaire sur l'échec de l'exercice

--- 9-09-signed_int.c
+++ 9-09-signed_int.c
@@ -10,4 +10,5 @@ main()
 /*
 Tentative pas réussie
 de comprendre des aspects des limites liées à l'encodage de l'information.
+C'est parce que 'int' est, de base, une valeur bien plus grande que l'intervalle défini par 127. Le problème est bien visible, par contre, lorsque l'on essaie de coder un nombre de téléphone sur un entier.
 */
