09f97932c490fd1382fe406a6837e8467e53a223
Daniele Pitrolo <d.pitrolo@gmx.com>
2015-12-06 11:41:23 +0100

    Ajout de la fonction pow pour voir si elle cause un message d'erreur lors de l'édition des liens

--- 12-06-1-erreurs.c
+++ 12-06-1-erreurs.c
@@ -5,4 +5,5 @@ main()
 {
   printf("Hello World") ; /*l'absence d'éléments syntactiques, comme le ';', donne également lieu à des erreurs syntactiques*/
   printf("cosinus = %f\n", cos(0.5));
+  printf("%d", pow(2));
 }
