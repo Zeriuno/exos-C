022d2e9d1c79017da253707572f92c8a69a88ce7
Daniele Pitrolo <d.pitrolo@gmx.com>
2015-12-06 11:29:47 +0100

    Fichier qui génère de multiples messages d'erreurs lors des différentes étapes vers le programme complet

---
+++ 12-06-1-erreurs.c
@@ -0,0 +1,8 @@
+/*Ce fichier, écrit pour montrer les différentes erreurs possibles entre l'écriture et l'exécution d'un programme, est de M. Léry, tous droits réservés*/
+#include <stdio.h> /*Si on écrit uniquement 'stdio', cela donne lieu à une erreur de compilation (ou erreur de syntaxe)*/
+
+main()
+{
+  printf("Hello World") ; /*l'absence d'éléments syntactiques, comme le ';', donne également lieu à des erreurs syntactiques*/
+  printf("cosinus = %f\n", cos(0.5));
+}
