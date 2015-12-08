1dce0f2db2e4d780dabc5d40cbcbeccdbedd6ba8
Daniele Pitrolo <d.pitrolo@gmx.com>
2015-12-08 17:43:14 +0100

    Erreur avec une merveilleuse boucle infinie

---
+++ 12-08-4-contrôle.c
@@ -0,0 +1,25 @@
+/*
+Code de M. Léry, noms des variables différents
+*/
+
+#include <stdio.h>
+main()
+{
+  int impairs, somme, i, j             ;
+
+  printf("Nombre de valeurs : ")       ;
+  scanf("%d", &impairs)                ;
+
+  i = -1                               ;
+  somme = 0                            ;
+  j = 0                                ;
+  while(j < impairs)/*erreur: j'ai mis '<='*/
+  {
+    i += 2                             ;
+    somme += i                         ;
+    j = j++                            ;
+    printf("i %d -- somme %d -- j %d", i, somme, j) ;
+  };
+
+  printf("la somme est : %d\n", somme) ;/*erreur: j'ai oublié '\n'*/
+}
