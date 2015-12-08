55b532c7af04dc36c8f5c48552df0d20b3bf02a7
Daniele Pitrolo <d.pitrolo@gmx.com>
2015-12-08 13:09:46 +0100

    Boucle afin de garantir que n2 ne soit pas 0

--- 12-08-1-opérations.c
+++ 12-08-1-opérations.c
@@ -4,16 +4,21 @@ main()
   int n1, n2, add, sous, mult, div, rest ;
 
   printf("Saisir un premier nombre, entier : ") ;
-  scanf("%d", &n1)      ;
-  printf("Un autre : ") ;
-  scanf("%d", &n2)      ;
-  if(n2=0)
+  scanf("%d", &n1)        ;
+
+  n2 = 0                  ;
+  /*initialisation à 0 afin de rentrer dans la boucle*/
+  while(n2 = 0)
+  {
+    printf("Un autre : ") ;
+    scanf("%d", &n2)      ;
+  }
   printf("Je vais faire les quatre opérations avec ces deux nombres...") ;
 
   /*
   suite de affectation et affichage afin de rendre plus compact le traitement de l'exception n2=0
   */
-  
+
   add = n1 + n2           ;
   printf("%d + %d = %d", n1, n2, add)  ;
   sous = n1 -n2           ;
@@ -22,8 +27,8 @@ main()
   printf("%d * %d = %d", n1, n2, mult) ;
   if(n2 != 0)
   {
-    div = n1 / n2           ;
-    rest = n1 % n2          ;
+    div = n1 / n2         ;
+    rest = n1 % n2        ;
     printf("%d / %d = %d avec reste %d", n1, n2, div, rest) ;
   }
 }
