6190cb7dc02b91d5322fbee66e726c49ebd3c462
Daniele Pitrolo <d.pitrolo@gmx.com>
2015-12-13 18:50:11 +0100

    Advent of code, Day 1, Part 2 - http://adventofcode.com/day/1/

--- 12-13-2-santa-1.c
+++ 12-13-2-santa-1.c
@@ -3,15 +3,18 @@ main()
 {
   FILE *a                                              ;
   char b                                               ;
-  int floors                                           ;
+  int floors, basement, i                              ;
 
   b = 'a'                                              ;
   floors = 0                                           ;
+  i = 0                                                ;
+  basement = 0                                         ;
 
   a = fopen("12-13-1-santa-1-instructions.txt", "r")   ;
   while(b != '\n')
   {
     fscanf(a, "%c", &b)                                ;
+    i++                                                ;
     switch(b)
     {
       case '(':
@@ -27,7 +30,10 @@ main()
         printf("\n\n\nHuston, we have a problem.\n\n") ;
         break                                          ;
     }
+    if((floors == -1) && (basement == 0))
+      basement = i                                     ;
   }
   fclose(a)                                            ;
   printf("Santa Claus is on floor n. %d\n", floors)    ;
+  printf("And he first entered the basement at position n.%d\n", basement) ;
 }
