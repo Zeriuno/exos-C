869495f55b528d7ce1735ff76de4530b3dc70235
Daniele Pitrolo <d.pitrolo@gmx.com>
2015-12-13 19:28:43 +0100

    Modularisation: la saisie du nom du fichier est dans une fonction à part

--- 12-13-2-santa-1.c
+++ 12-13-2-santa-1.c
@@ -1,20 +1,24 @@
 /**/
-
 #include <stdio.h>
+
+char saisie_fichier() ;
+
+FILE *a               ;
+char file[20]         ;
+
 main()
 {
-  FILE *a                                              ;
-  char b, file[20]                                     ;
   int floors, basement, i                              ;
+  char b                                               ;
 
   b = 'a'                                              ;
   floors = 0                                           ;
   i = 0                                                ;
   basement = 0                                         ;
 
-  printf("What's the name of the file to count the floors? ") ;
-  scanf("%s", file)                                    ;
+  file[20] = saisie_fichier()                          ;
   a = fopen(file, "r")                                 ;
+
   while(b != '\n')
   {
     fscanf(a, "%c", &b)                                ;
@@ -40,4 +44,24 @@ main()
   fclose(a)                                            ;
   printf("Santa Claus is on floor n. %d\n", floors)    ;
   printf("And he first entered the basement at position n.%d\n", basement) ;
+}
+
+
+/*---------------------------------------------------------
+--                Saisie du fichier                      --
+--                                                       --
+--                                                       --
+--                                                       --
+--                                                       --
+--                                                       --
+--                                                       --
+--                                                       --
+---------------------------------------------------------*/
+
+
+char saisie_fichier()
+{
+  printf("What's the name of the file to count the floors? ") ;
+  scanf("%s", file)                                           ;
+  return file[20]                                             ;
 }
