4bf48d260ec9446cec2944adfd312659473b11a6
Daniele Pitrolo <d.pitrolo@gmx.com>
2015-12-15 18:00:04 +0100

    Crée un menu pour choisir quelle fonctionnalité du programme lancer

--- 12-11-2-saisie-tableau-mod.c
+++ 12-11-2-saisie-tableau-mod.c
@@ -1,21 +1,57 @@
 /*
 Exercice fait par M. Léry, tous droits réservés
-Saisie de notes dans un tableau, '-1' arrête la saisie*/
+Saisie de notes dans un tableau, '-1' arrête la saisie
+
+Jeu de tests
+19 20 10 10 0 1 15 -1
+*/
 #include <stdio.h>
 #define MAX_NOTES 20
-void saisie()                              ;
-void affichage(), min_max()                ;
-float moyenne_calcul()                     ;
-float min, max, tabnotes[MAX_NOTES]        ;
-int nbnotes                                ;
+void saisie()                                     ;
+void affichage(), min_max()                       ;
+float moyenne_calcul()                            ;
+float min, max, tabnotes[MAX_NOTES]               ;
+int nbnotes                                       ;
 main()
 {
-  float moyenne_main                       ;
-  saisie()                                 ;
-  affichage()                              ;
-  moyenne_main = moyenne_calcul()          ;
-  printf("Moyenne : %6.2f\n", moyenne_main);
-  min_max()                                ;
+  float moyenne_main                              ;
+  int choix                                       ;
+
+  choix = 9999                                    ;
+  while(choix !=0)
+  {
+    printf("\n-1- Saisie\n")                      ;
+    printf("-2- Affichage\n")                     ;
+    printf("-3- Moyenne\n")                       ;
+    printf("-4- min et MAX\n")                    ;
+    printf("-0- Quitter\n\n")                     ;
+    printf("Choix : ")                            ;
+
+    scanf("%d", &choix)                           ;
+    printf("\n\n")                                ;
+    switch(choix)
+    {
+      case 0:
+        printf("Au revoir.")                      ;
+        break                                     ;
+      case 1:
+        saisie()                                  ;
+        break                                     ;
+      case 2:
+        affichage()                               ;
+        break                                     ;
+      case 3:
+        moyenne_main = moyenne_calcul()           ;
+        printf("Moyenne : %6.2f\n", moyenne_main) ;
+        break                                     ;
+      case 4:
+        min_max()                                 ;
+        break                                     ;
+      default:
+        printf("Erreur de saisie")                ;
+        break                                     ;
+    }
+  }
 }
 
 
