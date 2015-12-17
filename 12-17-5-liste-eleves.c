e3f491d134d249af2ccfe1ba7b1f7ebd364fa430
Daniele Pitrolo <d.pitrolo@gmx.com>
2015-12-17 18:27:52 +0100

    Procédure de saisie

--- 12-17-5-liste-eleves.c
+++ 12-17-5-liste-eleves.c
@@ -7,6 +7,7 @@ Code de M. Léry
 
 #include <stdio.h>
 #include <ctype.h>
+#include <string.h>
 
 #define TAILLE 200
 
@@ -20,7 +21,7 @@ struct etudiant {
 
 
 /* Variable globale */
-struct etudiant eleve[TAILLE]                            ;
+struct etudiant tabeleve[TAILLE]                         ;
 int nbeleves = 0                                         ;
 
 /*----------------------------------
@@ -83,7 +84,32 @@ Saisie
 
 void saisie()
 {
-  printf("Saisie.") ;
+  const MAXNOM = 200 ;
+  const MAXPRENOM = 200 ;
+  char nom[MAXNOM], prenom[MAXPRENOM] ;
+
+  strcpy(eleve.nom, "début")          ;
+
+  while(strcmp(eleve.nom,"!") != 0)
+  {
+    printf("--------------------") ;
+    printf("Saisie d'un étudiant") ;
+    printf("--------------------") ;
+    printf("Nom ('!' pour terminer): ") ;
+    printf("%s", eleve.nom)     ;
+    if(nom != '!\n')
+    {
+      printf("Prénom : ")       ;
+      scanf("%s", eleve.prenom) ;
+      printf("Âge : ")          ;
+      scanf("%d", eleve.age)    ;
+
+      printf("\n")              ;
+      tabeleve[i++] = eleve
+    }
+  }
+  nbeleves = i ;
+  printf("%d élèves saisis.", nbeleves) ;
 }
 
 /*----------------------------------
