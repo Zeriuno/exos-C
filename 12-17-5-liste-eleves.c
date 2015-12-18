0e1c68e9d60adabe762645cb6d2c7280a5bbc88c
Daniele Pitrolo <d.pitrolo@gmx.com>
2015-12-18 17:45:04 +0100

    MAXNOM en constante globale

--- 12-17-5-liste-eleves.c
+++ 12-17-5-liste-eleves.c
@@ -12,6 +12,7 @@ Code de M. Léry
 #define TAILLE 200
 #define NON_TROUVE -1 /*Ceci afin de rendre plus compréhensible le code à la simple lecture*/
 /* Type global */
+#define MAXNOM 200 ; /* de moi */
 
 struct etudiant {
   char nom[20]                                           ;
@@ -99,7 +100,6 @@ Saisie
 
 void saisie()
 {
-  const MAXNOM = 200 ; /* de moi */
   const MAXPRENOM = 200 ; /* de moi */
   char nom[MAXNOM], prenom[MAXPRENOM] ;
   struct etudiant eleve ;
