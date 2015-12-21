82277285f1da93bcc44d5857bd735b7167063399
Zeriuno <d.pitrolo@gmx.com>
2015-12-22 00:11:34 +0100

    Note sur comment gérer les espaces dans une chaîne de caractères dans un fichier

---
+++ 12-18-3-gerer-espaces-fichier.c
@@ -0,0 +1,5 @@
+/*
+stocker le nom avec tirets ou bien utiliser des séparateurs entre les champs
+Si tirets, alors encoder avec tirets en sauvegardant dans le fichier et enlever les tirets losqu'on charge en mémoire
+Sinon, avec séparateurs, il faudra lire caractère par caractère jusqu'au séparateur.
+*/
