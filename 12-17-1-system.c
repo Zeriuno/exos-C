68b2133fa665bcbe60e829b724c1921603a2e2a6
Zeriuno <d.pitrolo@gmx.com>
2015-12-21 23:55:49 +0100

    Les fonctions concernant les chaînes de caractères ne sont plus nécessaires

--- 12-17-1-system.c
+++ 12-17-1-system.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 main()
 {
   char uuid[36]                ;
@@ -10,7 +9,5 @@ main()
   f = popen("uuidgen", "r")    ;
   fscanf(f, "%s", uuid)        ;
   pclose(f)                    ;
-  luuid = strlen(uuid)         ;
   printf("UUID = %s\n", uuid)  ;
-  printf("UUID = %d\n", luuid) ;
 }
