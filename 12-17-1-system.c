48e50ab6e1f935668594d0ac0e29b749d4ab11be
Zeriuno <d.pitrolo@gmx.com>
2015-12-21 23:54:41 +0100

    Calcul de la bonne longueur du UUID

--- 12-17-1-system.c
+++ 12-17-1-system.c
@@ -1,11 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 main()
 {
-  char uuid[100]                  ;
-  FILE *f ;
+  char uuid[36]                ;
+  int luuid                    ;
+  FILE *f                      ;
 
-  f = popen("uuidgen", "r") ;
-  fscanf(f, "%s", uuid)     ;
-  pclose(f) ;
+  f = popen("uuidgen", "r")    ;
+  fscanf(f, "%s", uuid)        ;
+  pclose(f)                    ;
+  luuid = strlen(uuid)         ;
+  printf("UUID = %s\n", uuid)  ;
+  printf("UUID = %d\n", luuid) ;
 }
