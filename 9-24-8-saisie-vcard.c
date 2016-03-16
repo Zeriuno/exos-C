930767e0ce208b9ad6884825d2b9c7cb8bc5c4ec
Zeriuno <d.pitrolo@gmx.com>
2016-03-16 10:26:18 +0100

    VCard avec notes, avec liste d'améliorations

--- 9-24-8-saisie-vcard.c
+++ 9-24-8-saisie-vcard.c
@@ -1,10 +1,20 @@
+/*
+
+Da fare:
+
+* BDAY:19840427
+* Caricare una fotografia
+
+*/
+
 #include <stdio.h>
 #include <string.h>
 main()
 {
-  char c, prenom[100], nom[100], mel[100], fichier[204], tel[20] ;
-  int i=0                                                        ;
-  FILE *vcard                                                    ;
+  char c, prenom[100], nom[100], mel[100]        ;
+  char fichier[204], note[100], tel[20]          ;
+  int i=0                                        ;
+  FILE *vcard                                    ;
 
   printf("Nous allons créer une vCard\n")        ;
   printf("Tu vas écrire les informations\n")     ;
@@ -41,11 +51,20 @@ main()
     i++                                          ;
   }
   tel[i+1] = '\0'                                ;
+  i = 0                                          ;
+  printf("Saisi le champ 'note' : ")             ;
+  while((c=getchar()) != '\n')
+  {
+    note[i] = c                                  ;
+    i++                                          ;
+  }
+  note[i+1] = '\0'                               ;
+
   strcat(fichier,nom)                            ;
   strcat(fichier,prenom)                         ;
   strcat(fichier,".vcf")                         ;
   vcard = fopen(fichier, "w")                    ;
-  fprintf(vcard, "BEGIN:VCARD\nVERSION:3.0\nN:%s;%s;;;\nFN:%s %s\nORG:\nROLE:\nEMAIL;type=INTERNET:%s\nTEL;type=VOICE:+%s\nNOTE:\nEND:VCARD", nom, prenom, prenom, nom, mel, tel) ;
+  fprintf(vcard, "BEGIN:VCARD\nVERSION:3.0\nN:%s;%s;;;\nFN:%s %s\nORG:\nROLE:\nEMAIL;type=INTERNET:%s\nTEL;type=VOICE:+%s\nNOTE:%s\nEND:VCARD", nom, prenom, prenom, nom, mel, tel, note) ;
   fclose(vcard)  ;
   printf("Le fichier %s a bien été créé\n", fichier) ;
 }
