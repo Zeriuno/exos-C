dc3ca4492141eee7dc76ce1383cfaa8fd3f19380
Zeriuno <d.pitrolo@gmx.com>
2016-03-29 01:09:44 +0200

    Intégration de l'UID

--- 9-24-8-saisie-vcard.c
+++ 9-24-8-saisie-vcard.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
+
 main()
 {
-  char c, prenom[100], nom[100], mel[100], fichier[204], tel[20] ;
-  int i=0                                                        ;
-  FILE *vcard                                                    ;
+  char c, prenom[100], nom[100], mel[100]        ;
+  char fichier[204], note[100], tel[20], uid[30] ;
+  int i=0                                        ;
+  FILE *vcard, *f1                               ;
 
   printf("Nous allons créer une vCard\n")        ;
   printf("Tu vas écrire les informations\n")     ;
@@ -41,11 +44,31 @@ main()
     i++                                          ;
   }
   tel[i+1] = '\0'                                ;
+  i = 0                                          ;
+  printf("Saisi le champ 'note' : ")             ;
+  while((c=getchar()) != '\n')
+  {
+    note[i] = c                                  ;
+    i++                                          ;
+  }
+  note[i+1] = '\0'                               ;
+
+  f1 = popen("uuidgen", "r")                     ;
+  if(f1 == NULL)
+  {
+    printf("Pas de UUID, erreur de popen\n")     ;
+  }
+  else
+  {
+    fscanf(f1, "%s", uid)                        ;
+    pclose(f1)                                   ;
+  }
+
   strcat(fichier,nom)                            ;
   strcat(fichier,prenom)                         ;
   strcat(fichier,".vcf")                         ;
   vcard = fopen(fichier, "w")                    ;
-  fprintf(vcard, "BEGIN:VCARD\nVERSION:3.0\nN:%s;%s;;;\nFN:%s %s\nORG:\nROLE:\nEMAIL;type=INTERNET:%s\nTEL;type=VOICE:+%s\nNOTE:\nEND:VCARD", nom, prenom, prenom, nom, mel, tel) ;
+  fprintf(vcard, "BEGIN:VCARD\nVERSION:3.0\nN:%s;%s;;;\nFN:%s %s\nORG:\nROLE:\nEMAIL;type=INTERNET:%s\nTEL;type=VOICE:+%s\nNOTE:%s\nUID:%s\nEND:VCARD", nom, prenom, prenom, nom, mel, tel, note, uid) ;
   fclose(vcard)  ;
   printf("Le fichier %s a bien été créé\n", fichier) ;
 }
