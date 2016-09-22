3faf81aec1de881a269aab94be65d7b62313e64d
Zeriuno <d.pitrolo@gmx.com>
2016-09-22 17:46:37 +0200

    Cast int to char marche

--- 2016-09-22-cast.c
+++ 2016-09-22-cast.c
@@ -2,13 +2,14 @@
 
 int main()
 {
-	int a ;
-	char b ;
+	int a, cast                    ;
+	char b                         ;
 
 	printf("Saisir un chiffre : ") ;
 	scanf("%d", &a)                ;
-	printf("Letto %d\n", a) ;
-	b = a - 48;
-	printf("Convertito %c\n", b) ;
-	return 0 ;
+	printf("Letto %d\n", a)        ;
+	cast = a + 48                  ;
+	b = cast                       ;
+	printf("Convertito %c\n", b)   ;
+	return 0                       ;
 }
