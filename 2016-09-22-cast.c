8d5d05de528dee2b09ce1951596210cad04dacab
Zeriuno <d.pitrolo@gmx.com>
2016-09-22 17:47:24 +0200

    Cast int to char mieux

--- 2016-09-22-cast.c
+++ 2016-09-22-cast.c
@@ -2,14 +2,13 @@
 
 int main()
 {
-	int a, cast                    ;
+	int a                          ;
 	char b                         ;
 
 	printf("Saisir un chiffre : ") ;
 	scanf("%d", &a)                ;
 	printf("Letto %d\n", a)        ;
-	cast = a + 48                  ;
-	b = cast                       ;
+	b =  a + 48                    ;
 	printf("Convertito %c\n", b)   ;
 	return 0                       ;
 }
