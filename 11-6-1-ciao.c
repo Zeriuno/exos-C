be582d66b1bb86c906baf8c75c76c391184f5284
Zeriuno <d.pitrolo@gmx.com>
2016-11-06 21:28:55 +0100

    Tests

--- 11-6-1-ciao.c
+++ 11-6-1-ciao.c
@@ -4,10 +4,11 @@ int main (int argc, char *  argv[])
 {
   if(argc == 2)
   {
-    printf("Ciao %s!\n", argv[1]);
-    printf("argc = %d\n", argc) ;
+    printf("Ciao %s!\n", argv[1])   ;
+    printf("argc = %d\n", argc)     ;
+    printf("argv[0] = %s\n", argv[0]) ;
   }
   else
-    printf("Ciao :-)") ;
-  return 0 ;
+    printf("Ciao :-)\n")              ;
+  return 0                          ;
 }
